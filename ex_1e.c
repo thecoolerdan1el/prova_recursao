96f13186347eca009ab49abb765a9b85722a2b17
Daniel <134326067+thecoolerdan1el@users.noreply.github.com>
2023-10-26 11:47:47 -0300

    Create ex_1e.c

---
+++ ex_1e.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int recursivo(int n) {
+  int resultado;
+
+  if (n == 1) {
+    resultado = 3;
+  } else if (n == 2) {
+    resultado = 5;
+  } else {
+    resultado = (n-1) * recursivo(n - 1) + (n - 2) * recursivo(n - 2);
+  }
+
+  return resultado;
+}
+
+int main() {
+  int n, resultado;
+
+  printf("Entre com o número N: ");
+  scanf("%d", &n);
+
+  resultado = recursivo(n);
+  printf("%d\n", resultado);
+
+  return 0;
+}
